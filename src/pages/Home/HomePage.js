60263469d0e0b2a406c1bb728bc9f8bcf057f684
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-01-25 03:56:35 +0530

    feat: test data fetching from api

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -4,16 +4,38 @@ import classes from "./HomePage.module.css";
 
 import { Col, Container, Row } from "react-grid-system";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+
 function HomePage (){
-    console.log('HOME>>>');
+    const [shows, setShows] = useState([]);
+    const [isLoading, setLoading] = useState(false);
+    const [httpError, setHttpError] = useState();
+
    const navigate = useNavigate();
+
+   const  getShowsData = async () => {
+    setLoading(true);
+    const response = await fetch('https://api.tvmaze.com/search/shows?q=girls');
+    if(!response.ok){
+      throw new Error("Something went wrong!");
+    }
+    const showData = await response.json();
+    setShows(showData);
+    setLoading(false);
+  }
     const handleSearch = () => {
         //TODO search
-        console.log('Search>>>');
-        navigate('/search');
-    }
+        getShowsData().catch((error) => {
+            setLoading(false);
+            setHttpError(error.message);
+          });
+        }
+
     return(
         <div className={classes["home-container"]}>
+            { isLoading &&  <p>Loading...</p>}
+            { httpError && httpError}
         <Container>
             <Row justify="center" align="center">
             <Col sm={6}  style={{ marginTop: '20%'}} align="center">
@@ -25,6 +47,7 @@ function HomePage (){
 
         </Row>
         </Container>
+        {console.log('Show List',shows)}
         </div>
     );
 
